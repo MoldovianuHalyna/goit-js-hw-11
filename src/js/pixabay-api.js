79abb0b522312d79b74dca29c3001a85111f2907
Halyna Moldovianu <Beorgeos@gmail.com>
2025-03-13 18:25:50 +0100

    12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,18 @@
-// functions that do the http request
+// function that gets data from server
+export const getPhotoFromServer = function (dataName) {
+  const searchParametters = new URLSearchParams({
+    key: '49322620-5f00be843ea1be6db390f9d83',
+    q: dataName,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  return fetch(`https://pixabay.com/api/?${searchParametters}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+};
